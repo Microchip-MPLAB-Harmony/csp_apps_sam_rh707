3a10b6357874466ef0ab798c475027620d57efa7
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-22 23:38:34 +0530

    [MCAN] Regenerated demo for mcan_mcan_fd_operation_blocking_sam_rh707_ek
    MH3-68276
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/mcan/mcan_fd_operation_blocking/firmware/src/config/sam_rh707_ek/peripheral/mpu/plib_mpu.c
+++ apps/mcan/mcan_fd_operation_blocking/firmware/src/config/sam_rh707_ek/peripheral/mpu/plib_mpu.c
@@ -56,8 +56,8 @@ void MPU_Initialize(void)
     /*** Configure MPU Regions  ***/
 
     /* Region 0 Name: ram_nocache, Base Address: 0x21010000, Size: 128KB  */
-    MPU->RBAR = MPU_REGION(0, 0x21010000);
-    MPU->RASR = MPU_REGION_SIZE(16) | MPU_RASR_AP(MPU_RASR_AP_READWRITE_Val) | MPU_ATTR_NORMAL \
+    MPU->RBAR = MPU_REGION(0U, 0x21010000U);
+    MPU->RASR = MPU_REGION_SIZE(16U) | MPU_RASR_AP(MPU_RASR_AP_READWRITE_Val) | MPU_ATTR_NORMAL \
                 | MPU_ATTR_ENABLE  ;
 
 
