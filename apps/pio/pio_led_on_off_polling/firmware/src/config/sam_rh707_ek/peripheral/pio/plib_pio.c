5a428d3eb03d57d8b2d8418c6dc4d59a45d4c8f1
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-22 23:48:06 +0530

    [PIO] Regenerated demo for pio_pio_led_on_off_polling_sam_rh707_ek
    MH3-68280
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/pio/pio_led_on_off_polling/firmware/src/config/sam_rh707_ek/peripheral/pio/plib_pio.c
+++ apps/pio/pio_led_on_off_polling/firmware/src/config/sam_rh707_ek/peripheral/pio/plib_pio.c
@@ -44,6 +44,8 @@
 #include "plib_pio.h"
 #include "interrupts.h"
 
+
+
 /******************************************************************************
   Function:
     PIO_Initialize ( void )
@@ -59,22 +61,22 @@ void PIO_Initialize ( void )
  /* Port A Peripheral function GPIO configuration */
 	PIOA_REGS->PIO_MSKR = 0x80000000LU;
 	PIOA_REGS->PIO_CFGR = 0x0U;
-	
+
  /* Port A Pin 31 configuration */
 	PIOA_REGS->PIO_MSKR = 0x80000000LU;
 	PIOA_REGS->PIO_CFGR = (PIOA_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x200U;
-	
+
  /* Port A Latch configuration */
 	PIOA_REGS->PIO_CODR = 0x80000000LU;
 
  /* Port B Peripheral function GPIO configuration */
 	PIOB_REGS->PIO_MSKR = 0x800U;
 	PIOB_REGS->PIO_CFGR = 0x0U;
-	
+
  /* Port B Pin 11 configuration */
 	PIOB_REGS->PIO_MSKR = 0x800U;
 	PIOB_REGS->PIO_CFGR = (PIOB_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
-	
+
  /* Port B Latch configuration */
 	PIOB_REGS->PIO_CODR = 0x800U;
 
@@ -119,6 +121,7 @@ uint32_t PIO_PortRead(PIO_PORT port)
     return PIO_REGS->PIO_GROUP[port].PIO_PDSR;
 }
 
+
 // *****************************************************************************
 /* Function:
     void PIO_PortWrite (PIO_PORT port, uint32_t mask, uint32_t value);
@@ -135,6 +138,7 @@ void PIO_PortWrite(PIO_PORT port, uint32_t mask, uint32_t value)
     PIO_REGS->PIO_GROUP[port].PIO_ODSR = value;
 }
 
+
 // *****************************************************************************
 /* Function:
     uint32_t PIO_PortLatchRead ( PIO_PORT port )
@@ -149,6 +153,7 @@ uint32_t PIO_PortLatchRead(PIO_PORT port)
 {
     return PIO_REGS->PIO_GROUP[port].PIO_ODSR;
 }
+
 
 // *****************************************************************************
 /* Function:
@@ -210,7 +215,7 @@ void PIO_PortToggle(PIO_PORT port, uint32_t mask)
 void PIO_PortInputEnable(PIO_PORT port, uint32_t mask)
 {
     PIO_REGS->PIO_GROUP[port].PIO_MSKR = mask;
-    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;	
+    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;
 }
 
 // *****************************************************************************
