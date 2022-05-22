737712b0ba863bbccf4c222942ce7312a6d18d59
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-22 23:23:22 +0530

    [CLOCK] Regenerated demo for clock_clock_config_sam_rh707_ek
    MH3-68268
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/clock/clock_config/firmware/src/config/sam_rh707_ek/peripheral/nvic/plib_nvic.c
+++ apps/clock/clock_config/firmware/src/config/sam_rh707_ek/peripheral/nvic/plib_nvic.c
@@ -60,7 +60,13 @@ void NVIC_Initialize( void )
     /* Enable the interrupt sources and configure the priorities as configured
      * from within the "Interrupt Manager" of MHC. */
 
+    /* Enable Usage fault */
+    SCB->SHCSR |= (SCB_SHCSR_USGFAULTENA_Msk);
+    /* Trap divide by zero */
+    SCB->CCR   |= SCB_CCR_DIV_0_TRP_Msk;
 
+    /* Enable Bus fault */
+    SCB->SHCSR |= (SCB_SHCSR_BUSFAULTENA_Msk);
 
 }
 
