b36b9fdf411db887fada601cd49f5a76e4034449
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-22 23:17:28 +0530

    [ADC] Regenerated demo for adc_adc_polled_mode_sam_rh707_ek
    MH3-68265
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/adc/adc_polled_mode/firmware/src/config/sam_rh707_ek/peripheral/pio/plib_pio.c
+++ apps/adc/adc_polled_mode/firmware/src/config/sam_rh707_ek/peripheral/pio/plib_pio.c
@@ -44,6 +44,8 @@
 #include "plib_pio.h"
 #include "interrupts.h"
 
+
+
 /******************************************************************************
   Function:
     PIO_Initialize ( void )
@@ -59,14 +61,14 @@ void PIO_Initialize ( void )
  /* Port A Peripheral function GPIO configuration */
 	PIOA_REGS->PIO_MSKR = 0x8000U;
 	PIOA_REGS->PIO_CFGR = 0x0U;
-	
+
  /* Port A Latch configuration */
 	PIOA_REGS->PIO_CODR = 0x8000U;
 
  /* Port B Peripheral function B configuration */
 	PIOB_REGS->PIO_MSKR = 0x30U;
 	PIOB_REGS->PIO_CFGR = 0x2U;
-	
+
 
 
 
@@ -109,6 +111,7 @@ uint32_t PIO_PortRead(PIO_PORT port)
     return PIO_REGS->PIO_GROUP[port].PIO_PDSR;
 }
 
+
 // *****************************************************************************
 /* Function:
     void PIO_PortWrite (PIO_PORT port, uint32_t mask, uint32_t value);
@@ -125,6 +128,7 @@ void PIO_PortWrite(PIO_PORT port, uint32_t mask, uint32_t value)
     PIO_REGS->PIO_GROUP[port].PIO_ODSR = value;
 }
 
+
 // *****************************************************************************
 /* Function:
     uint32_t PIO_PortLatchRead ( PIO_PORT port )
@@ -139,6 +143,7 @@ uint32_t PIO_PortLatchRead(PIO_PORT port)
 {
     return PIO_REGS->PIO_GROUP[port].PIO_ODSR;
 }
+
 
 // *****************************************************************************
 /* Function:
@@ -200,7 +205,7 @@ void PIO_PortToggle(PIO_PORT port, uint32_t mask)
 void PIO_PortInputEnable(PIO_PORT port, uint32_t mask)
 {
     PIO_REGS->PIO_GROUP[port].PIO_MSKR = mask;
-    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;	
+    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;
 }
 
 // *****************************************************************************
