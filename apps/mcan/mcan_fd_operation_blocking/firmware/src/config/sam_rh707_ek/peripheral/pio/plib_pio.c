1a0ddbc33c12e6e3ddea13db72c75e2d9538c796
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-04-17 14:38:42 +0530

    [MCAN] Regenerated demo for mcan_mcan_fd_operation_blocking_sam_rh707_ek
    MH3-79755

--- apps/mcan/mcan_fd_operation_blocking/firmware/src/config/sam_rh707_ek/peripheral/pio/plib_pio.c
+++ apps/mcan/mcan_fd_operation_blocking/firmware/src/config/sam_rh707_ek/peripheral/pio/plib_pio.c
@@ -59,23 +59,23 @@
 void PIO_Initialize ( void )
 {
  /* Port A Peripheral function GPIO configuration */
-	PIOA_REGS->PIO_MSKR = 0x800000U;
-	PIOA_REGS->PIO_CFGR = 0x0U;
+   PIOA_REGS->PIO_MSKR = 0x800000U;
+   PIOA_REGS->PIO_CFGR = 0x0U;
 
  /* Port A Pin 23 configuration */
-	PIOA_REGS->PIO_MSKR = 0x800000U;
-	PIOA_REGS->PIO_CFGR = (PIOA_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
+   PIOA_REGS->PIO_MSKR = 0x800000U;
+   PIOA_REGS->PIO_CFGR = (PIOA_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
 
  /* Port A Latch configuration */
-	PIOA_REGS->PIO_CODR = 0x800000U;
+   PIOA_REGS->PIO_CODR = 0x800000U;
 
  /* Port B Peripheral function A configuration */
-	PIOB_REGS->PIO_MSKR = 0x180U;
-	PIOB_REGS->PIO_CFGR = 0x1U;
+   PIOB_REGS->PIO_MSKR = 0x180U;
+   PIOB_REGS->PIO_CFGR = 0x1U;
 
  /* Port B Peripheral function B configuration */
-	PIOB_REGS->PIO_MSKR = 0x30U;
-	PIOB_REGS->PIO_CFGR = 0x2U;
+   PIOB_REGS->PIO_MSKR = 0x30U;
+   PIOB_REGS->PIO_CFGR = 0x2U;
 
 
 
