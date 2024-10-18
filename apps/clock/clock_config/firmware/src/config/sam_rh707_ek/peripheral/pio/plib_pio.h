8d24210dbc9eae3bc67a1570bf794e9b93c027ae
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-18 12:40:11 +0530

    [CLOCK] Regenerated demo for clock__clock_config__firmware__sam_rh707_ek.X
    MH3-102390

--- apps/clock/clock_config/firmware/src/config/sam_rh707_ek/peripheral/pio/plib_pio.h
+++ apps/clock/clock_config/firmware/src/config/sam_rh707_ek/peripheral/pio/plib_pio.h
@@ -91,21 +91,21 @@
 
 
 /*** Macros for LED0 pin ***/
-#define LED0_Set()               (PIOB_REGS->PIO_SODR = (1<<11))
-#define LED0_Clear()             (PIOB_REGS->PIO_CODR = (1<<11))
+#define LED0_Set()               (PIOB_REGS->PIO_SODR = ((uint32_t)1U<<11U))
+#define LED0_Clear()             (PIOB_REGS->PIO_CODR = ((uint32_t)1U<<11U))
 #define LED0_Toggle()            do {\
-                                            PIOB_REGS->PIO_MSKR = (1<<11); \
-                                            PIOB_REGS->PIO_ODSR ^= (1<<11);\
+                                            PIOB_REGS->PIO_MSKR = ((uint32_t)1U<<11U); \
+                                            PIOB_REGS->PIO_ODSR ^= ((uint32_t)1U<<11U);\
                                         } while (0)
 #define LED0_OutputEnable()      do {\
-                                            PIOB_REGS->PIO_MSKR = (1<<11); \
-                                            PIOB_REGS->PIO_CFGR |=(1 << PIO_CFGR_DIR_Pos);\
+                                            PIOB_REGS->PIO_MSKR = ((uint32_t)1U<<11U); \
+                                            PIOB_REGS->PIO_CFGR |=((uint32_t)1U << PIO_CFGR_DIR_Pos);\
                                         }while(0)
 #define LED0_InputEnable()       do { \
-                                            PIOB_REGS->PIO_MSKR = (1<<11); \
-                                            PIOB_REGS->PIO_CFGR &= ~(1 << PIO_CFGR_DIR_Pos);\
+                                            PIOB_REGS->PIO_MSKR = ((uint32_t)1U<<11U); \
+                                            PIOB_REGS->PIO_CFGR &= ~((uint32_t)1U << PIO_CFGR_DIR_Pos);\
                                         } while (0)
-#define LED0_Get()               ((PIOB_REGS->PIO_PDSR >> 11) & 0x1)
+#define LED0_Get()               ((PIOB_REGS->PIO_PDSR >> 11U) & 0x1U)
 #define LED0_PIN                  PIO_PIN_PB11
 // *****************************************************************************
 /* PIO Ports
